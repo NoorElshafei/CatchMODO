0c7666a9383a9cb5422570cab227e917c23b6c5c
Noor Elshafei <noor.elshafie@gmail.com>
2022-02-15 16:38:24 +0200

    six commit 12

--- app/src/main/java/com/bbi/catchmodo/ui/activities/TopUser.java
+++ app/src/main/java/com/bbi/catchmodo/ui/activities/TopUser.java
@@ -1,15 +1,15 @@
 package com.bbi.catchmodo.ui.activities;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import com.bbi.catchmodo.R;
 import com.bbi.catchmodo.data.model.RegisterModel;
@@ -22,26 +22,24 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class TopUser extends AppCompatActivity {
-    ActivityTopUserBinding binding;
-    TopUserAdapter adapter;
-    ArrayList<RegisterModel> allUser;
-    FirebaseUser firebaseUser;
-LinearLayout linearLayout;
-    DatabaseReference reference;
-    RegisterModel registerModel;
-    ProgressDialog progressDialog;
-    ArrayList<RegisterModel> TopUser;
-    RegisterModel userModel;
-    ArrayList<String> TopTenStrings;
-    int i;
+    private ActivityTopUserBinding binding;
+    private TopUserAdapter adapter;
+    private ArrayList<RegisterModel> allUser;
+    private FirebaseUser firebaseUser;
+    private LinearLayout linearLayout;
+    private DatabaseReference reference;
+    private RegisterModel registerModel;
+    private ProgressDialog progressDialog;
+    private ArrayList<RegisterModel> topUser;
+    private RegisterModel userModel;
+    private ArrayList<String> TopTenStrings;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +47,14 @@ LinearLayout linearLayout;
         binding = DataBindingUtil.setContentView(this, R.layout.activity_top_user);
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         allUser = new ArrayList<>();
-        TopTenStrings=new ArrayList<>();
-        linearLayout=findViewById(R.id.linearLayout6);
+        TopTenStrings = new ArrayList<>();
+        linearLayout = findViewById(R.id.linearLayout6);
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading....");
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.show();
         //   Glide.with(TopUser.this).load(R.drawable.logo_gif).into(binding.logo);
-        TopUser = new ArrayList();
+        topUser = new ArrayList();
         binding.back.setOnClickListener(view -> {
             onBackPressed();
         });
@@ -65,7 +63,7 @@ LinearLayout linearLayout;
         getTopUser();
 
 
-binding.constraint1.setVisibility(View.INVISIBLE);
+        binding.constraint1.setVisibility(View.INVISIBLE);
     }
 
 
@@ -76,9 +74,7 @@ binding.constraint1.setVisibility(View.INVISIBLE);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-
                 userModel = snapshot.getValue(RegisterModel.class);
-
 
             }
 
@@ -101,26 +97,24 @@ binding.constraint1.setVisibility(View.INVISIBLE);
                     registerModel = snapshot1.getValue(RegisterModel.class);
                     progressDialog.dismiss();
                     allUser.add(registerModel);
-
-
                 }
-
+                topUser.clear();
                 Collections.sort(allUser, (lhs, rhs) -> Integer.parseInt(rhs.getScore()) - Integer.parseInt(lhs.getScore()));
-                for ( i= 0; i <= 9; i++) {
+                for (i = 0; i <= 9; i++) {
 
-                    TopUser.add(allUser.get(i));
+                    topUser.add(allUser.get(i));
 
                     TopTenStrings.add(allUser.get(i).getId());
                 }
-                adapter = new TopUserAdapter(TopUser.this, TopUser);
+                adapter = new TopUserAdapter(TopUser.this, topUser);
                 binding.recycle.setAdapter(adapter);
                 if (!TopTenStrings.contains(firebaseUser.getUid())) {
-                    binding.name.setText(userModel.getUser_name()+'('+"You"+')');
+                    binding.name.setText(userModel.getUser_name() + '(' + "You" + ')');
                     binding.score.setText(userModel.getScore());
                     Glide.with(getApplicationContext()).load(userModel.getImage_url()).placeholder(R.drawable.moodo_icon).into(binding.image);
                     binding.constraint1.setVisibility(View.VISIBLE);
 
-                }else {
+                } else {
                     binding.constraint1.setVisibility(View.INVISIBLE);
 
                 }
